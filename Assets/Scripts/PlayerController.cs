9a85303465a40e37ae0853b51097a3362af49304
kasper <kasperfriangt@gmail.com>
2022-02-16 17:28:39 +0100

    added game win screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject WinTextObject;
+    private int count = 0;
     private Rigidbody playerRigidBody;
     private float movementX;
     private float movementZ;
@@ -13,6 +17,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        WinTextObject.SetActive(false);
+        setCountText();
     }
 
     // Update is called once per frame
@@ -21,6 +27,15 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVec = force.Get<Vector2>();
         movementX = movementVec.x;
         movementZ = movementVec.y;
+    }
+
+    void setCountText()
+    {
+        countText.text = "Gold cubes collected: " + count;
+        if (count>13)
+        {
+            WinTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
@@ -34,6 +49,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            setCountText();
         }
         
     }
